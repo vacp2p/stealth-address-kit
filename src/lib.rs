4b3d1bad5ab4c76b08921491ddaaf6a762f202fd
rymnc <43716372+rymnc@users.noreply.github.com>
2023-08-18 16:32:57 +0530

    chore: add rln test to demo membership rollup

--- src/lib.rs
+++ src/lib.rs
@@ -67,6 +67,12 @@ pub fn generate_stealth_private_key(
 mod tests {
     use super::*;
     use ark_ec::CurveGroup;
+    use rln::public::RLN;
+    use std::io::Cursor;
+    use ark_std::rand::thread_rng;
+    use color_eyre::{Report, Result};
+    use rln::utils::fr_to_bytes_le;
+    use serde_json::json;
 
     #[test]
     fn test_random_keypair() {
@@ -124,5 +130,42 @@ mod tests {
 
         let derived_commitment = derive_public_key(stealth_private_key_opt.unwrap());
         assert_eq!(derived_commitment, stealth_commitment);
+    }
+
+    #[test]
+    fn apply_stealth_membership_from_one_tree_to_another() -> Result<()> {
+        let test_tree_height = 20;
+        let resources = Cursor::new(json!({"resources_folder": "tree_height_20"}).to_string());
+        let mut rln = RLN::new(test_tree_height, resources.clone())?;
+
+        let alice_leaf = Fr::rand(&mut thread_rng());
+        let (alice_known_spending_sk, alice_known_spending_pk) = random_keypair();
+        let alice_leaf_buffer = Cursor::new(fr_to_bytes_le(&alice_leaf));
+        rln.set_leaf(0, alice_leaf_buffer)?;
+
+        // now the application sees that a user has been inserted into the tree
+        let mut rln_app_tree = RLN::new(test_tree_height, resources)?;
+        // the application generates a stealth commitment for alice
+        let (ephemeral_private_key, ephemeral_public_key) = random_keypair();
+        let (alice_stealth_commitment, view_tag) = generate_stealth_commitment(alice_known_spending_pk, alice_known_spending_pk, ephemeral_private_key);
+
+        let parts = [alice_stealth_commitment.x, alice_stealth_commitment.y];
+        let fr_parts = parts.map(|x| Fr::from(x.0));
+        let alice_stealth_commitment_buffer = Cursor::new(fr_to_bytes_le(&poseidon_hash(&fr_parts)));
+        rln_app_tree.set_leaf(0, alice_stealth_commitment_buffer)?;
+
+        // now alice's stealth commitment has been inserted into the tree, but alice has not
+        // yet derived the secret for it -
+        let alice_stealth_private_key_opt = generate_stealth_private_key(ephemeral_public_key, alice_known_spending_sk, alice_known_spending_sk, view_tag);
+        if alice_stealth_private_key_opt.is_none() {
+            return Err(Report::msg("Invalid view tag"));
+        }
+        let alice_stealth_private_key = alice_stealth_private_key_opt.unwrap();
+
+        assert_eq!(derive_public_key(alice_stealth_private_key), alice_stealth_commitment);
+
+        // now alice may generate valid rln proofs for the rln app tree, using a commitment
+        // derived from her commitment on the other tree
+        Ok(())
     }
 }
