5477c800e72caa5a4ab863001d0bcb61dc6404c0
rymnc <43716372+rymnc@users.noreply.github.com>
2024-07-06 17:28:01 +0400

    workshop: stage 2

--- sdk/src/bls12_381_on_g2_impl.rs
+++ sdk/src/bls12_381_on_g2_impl.rs
@@ -1,4 +1,4 @@
-use crate::stealth_addresses::StealthAddressOnCurve;
+use crate::{define_curve_tests, stealth_addresses::StealthAddressOnCurve};
 /// Implementation of the StealthAddressOnCurve trait for the Bls12_381 curve (G2).
 use ark_bls12_381::G2Projective;
 
@@ -7,3 +7,6 @@ struct Bls12_381_G2;
 impl StealthAddressOnCurve for Bls12_381_G2 {
     type Projective = G2Projective;
 }
+
+// define the tests for the curve
+define_curve_tests!(Bls12_381_G2);
