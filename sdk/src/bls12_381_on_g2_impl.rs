b5bf834f8295bd5ad4668972fcee0094a0faa678
rymnc <43716372+rymnc@users.noreply.github.com>
2024-07-06 17:26:21 +0400

    workshop: stage 1

--- sdk/src/bls12_381_on_g2_impl.rs
+++ sdk/src/bls12_381_on_g2_impl.rs
@@ -3,3 +3,7 @@ use crate::stealth_addresses::StealthAddressOnCurve;
 use ark_bls12_381::G2Projective;
 
 struct Bls12_381_G2;
+
+impl StealthAddressOnCurve for Bls12_381_G2 {
+    type Projective = G2Projective;
+}
